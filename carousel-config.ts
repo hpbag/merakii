b9cdf6907a0e31fe2f7fa59c869889ac8becbfb7
Harpriya Bagri <harpriyabagri@gmail.com>
2023-09-06 15:57:52 -0700

    reduced image sizes

--- carousel-config.ts
+++ carousel-config.ts
@@ -2,146 +2,146 @@ import { CarouselItemProps } from './components/CarouselItem';
 
 export const topRow: Record<string, CarouselItemProps> = {
   0: {
-    imgUrl: "1.jpg",
+    imgUrl: "/1.jpg",
     title: "Party"
   },
 
   1: {
-    imgUrl: "12.jpg",
+    imgUrl: "/12.jpg",
     title: "flower"
   },
 
   2: {
-    imgUrl: "4.jpg",
+    imgUrl: "/4.jpg",
     title: "pottery"
   },
 
   3: {
-    imgUrl: "8.jpg",
+    imgUrl: "/8.jpg",
     title: "Text 4"
   },
 
   4: {
-    imgUrl: "6.jpg",
+    imgUrl: "/6.jpg",
     title: "Text 5"
   },
 
   5: {
-    imgUrl: "7.jpg",
+    imgUrl: "/7.jpg",
     title: "Text 6"
   },
 
   6: {
-    imgUrl: "5.jpg",
+    imgUrl: "/5.jpg",
     title: "Text 7",
   },
   7: {
-    imgUrl: "9.jpg",
+    imgUrl: "/9.jpg",
     title: "Text 7",
   }, 
   8: {
-    imgUrl: "10.jpg",
+    imgUrl: "/10.jpg",
     title: "Text 7",
   }, 
   9: {
-    imgUrl: "11.jpg",
+    imgUrl: "/11.jpg",
     title: "Text 7",
   },
   10: {
-    imgUrl: "2.jpg",
+    imgUrl: "/2.jpg",
     title: "Text 7",
   },
   11: {
-    imgUrl: "13.jpg",
+    imgUrl: "/13.jpg",
     title: "Text 7",
   },
   12: {
-    imgUrl: "14.jpg",
+    imgUrl: "/14.jpg",
     title: "Text 7",
   },
   13: {
-    imgUrl: "15.jpg",
+    imgUrl: "/15.jpg",
     title: "Text 7",
   },
   14: {
-    imgUrl: "16.jpg",
+    imgUrl: "/16.jpg",
     title: "Text 7",
   },
   15: {
-    imgUrl: "17.jpg",
+    imgUrl: "/17.jpg",
     title: "Text 7",
   },
 }
 
 export const bottomRow: Record<string, CarouselItemProps> = {
   0: {
-    imgUrl: "19.jpeg",
+    imgUrl: "/19.jpeg",
     title: "Party"
   },
 
   1: {
-    imgUrl: "20.jpeg",
+    imgUrl: "/20.jpeg",
     title: "flower"
   },
 
   2: {
-    imgUrl: "21.jpeg",
+    imgUrl: "/21.jpeg",
     title: "pottery"
   },
 
   3: {
-    imgUrl: "22.jpeg",
+    imgUrl: "/22.jpeg",
     title: "Text 4"
   },
 
   4: {
-    imgUrl: "23.jpeg",
+    imgUrl: "/23.jpeg",
     title: "Text 5"
   },
 
   5: {
-    imgUrl: "24.jpeg",
+    imgUrl: "/24.jpeg",
     title: "Text 6"
   },
 
   6: {
-    imgUrl: "25.jpeg",
+    imgUrl: "/25.jpeg",
     title: "Text 7",
   },
   7: {
-    imgUrl: "26.jpeg",
+    imgUrl: "/26.jpeg",
     title: "Txt 7",
   }, 
   8: {
-    imgUrl: "28.jpeg",
+    imgUrl: "/28.jpeg",
     title: "Text 7",
   }, 
   9: {
-    imgUrl: "29.jpeg",
+    imgUrl: "/29.jpeg",
     title: "Text 7",
   },
   10: {
-    imgUrl: "1.jpg",
+    imgUrl: "/1.jpg",
     title: "Text 7",
   },
   11: {
-    imgUrl: "12.jpg",
+    imgUrl: "/12.jpg",
     title: "Text 7",
   },
   12: {
-    imgUrl: "4.jpg",
+    imgUrl: "/4.jpg",
     title: "Text 7",
   },
   13: {
-    imgUrl: "8.jpg",
+    imgUrl: "/8.jpg",
     title: "Text 7",
   },
   14: {
-    imgUrl: "6.jpg",
+    imgUrl: "/6.jpg",
     title: "Text 7",
   },
   15: {
-    imgUrl: "7.jpg",
+    imgUrl: "/7.jpg",
     title: "Text 7",
   },
 }
